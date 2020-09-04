d3c48a943663cb58556299bb58f8a351a0966cfa
aaron <aaron@192.168.1.17>
2020-09-04 12:40:47 -0500

    Este es un pequeño proyecto de un juego.

---
+++ src/dotgame/DotGame.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dotgame;
+
+/**
+ *
+ * @author aaron
+ */
+public class DotGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        int numOfGuesses=0;
+        
+        GameHelper helper = new GameHelper();
+        
+        ClassDotCom theDotCom = new ClassDotCom();
+        
+        int randomNum = (int) (Math.random()*5);
+        
+        int[] locations = {randomNum,randomNum+1,randomNum+2};
+        
+        theDotCom.setLocationCells(locations);
+        boolean isAlive=true;
+        
+        
+        while(isAlive == true){
+            
+            String guess = helper.getUserInput("enter a number");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if(result.equals("kill")){
+                isAlive= false;
+                System.out.println("You took "+ numOfGuesses + " guesses");
+            }
+        }
+        
+       
+    }
+    
+}
